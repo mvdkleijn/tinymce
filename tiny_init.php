a0d8335420ef7b20b5f24b6d94de55980bf28af2
mvdkleijn <martijn.niji@gmail.com>
2010-05-04 18:32:40 +0000

    Added latest tinymce version 3.3.4

--- tiny_init.php
+++ tiny_init.php
@@ -10,43 +10,48 @@ $pluginDir = dirname($_SERVER['PHP_SELF']);
 ?>
 
 tinyMCE.init({
-    mode : "none",
+    // Generic
 	theme : "advanced",
-	plugins : "table,fullscreen,preview,contextmenu,advimage,codeprotect,media,pagebreak",
-	theme_advanced_buttons1 : "bold,italic,underline,separator,strikethrough,justifyleft,justifycenter,justifyright, justifyfull,bullist,numlist,undo,redo,link,unlink,|,image,media,styleselect,formatselect,|,fullscreen,code",
-	theme_advanced_buttons2 : "pagebreak,|,tablecontrols,|,hr,removeformat,|,outdent,indent,blockquote,|,preview",
-	theme_advanced_buttons3 : "",
-	theme_advanced_toolbar_location : "top",
-	theme_advanced_toolbar_align : "left",
-	theme_advanced_statusbar_location : "bottom",
-	theme_advanced_resize_horizontal : false,
-	theme_advanced_resizing : true,
+	mode : "none",
 
-    // Page break seperator
-    pagebreak_separator : "<!-- read more -->",
-
-	// Some performance enhancements according to TinyMCE website
+    // Performance enhancements according to TinyMCE website
 	button_tile_map : true,
 
     // Fix for images showing up correctly in editor, but not in final page
     relative_urls : false,
     //document_base_url : "/",
 
-    file_browser_callback : "tinyBrowser",
-
-    //Example of how to add your stylesheet styles to the styles dropdown box in TinyMCE
-    //theme_advanced_styles : "Normal text=normaltext, Align left=align-left, Align right=align-right",
+    // Toolbar positioning
+    theme_advanced_toolbar_location : "top",
+    theme_advanced_toolbar_align : "left",
+	theme_advanced_statusbar_location : "bottom",
+	theme_advanced_resize_horizontal : false,
+	theme_advanced_resizing : true,
 
     // Preview content in system\'s style
     content_css : "<?php echo $pluginDir; ?>/config.php?g=css",
 
 	// Dropdown lists for link/image/media/template dialogs
-	//external_image_list_url : "<?php echo $pluginDir; ?>/lists/image_list.php",
+	external_image_list_url : "<?php echo $pluginDir; ?>/lists/image_list.php",
 	external_link_list_url : "<?php echo $pluginDir; ?>/lists/pages_list.php",
 
-	extended_valid_elements : "a[name|href|target|title|onclick|style],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name|style],hr[class|width|size|noshade|style],font[face|size|color|style],span[class|align|style]"
-});
+	extended_valid_elements : "a[name|href|target|title|onclick|style],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name|style],hr[class|width|size|noshade|style],font[face|size|color|style],span[class|align|style]",
 
-<?php
-include dirname(__FILE__).'/tinymce/jscripts/tiny_mce/plugins/tinybrowser/tb_tinymce.js.php';
-?>
+    // Plugins used
+    plugins : "table,fullscreen,preview,contextmenu,advimage,media,pagebreak,codeprotect",
+
+    // Plugin configs
+    // Plugin: Page break seperator
+    pagebreak_separator : "<!-- more -->",
+
+    // Toolbar layout
+	theme_advanced_buttons1 : "bold,italic,underline,separator,strikethrough,justifyleft,justifycenter,justifyright, justifyfull,bullist,numlist,undo,redo,link,unlink,|,image,media,styleselect,formatselect,|,fullscreen,code",
+	theme_advanced_buttons2 : "pagebreak,|,tablecontrols,|,hr,removeformat,|,outdent,indent,blockquote,|,preview",
+	theme_advanced_buttons3 : "",
+
+    //Example of how to add your stylesheet styles to the styles dropdown box in TinyMCE
+    //theme_advanced_styles : "Normal text=normaltext, Align left=align-left, Align right=align-right"
+
+
+    // file_browser_callback : "tinyBrowser",
+});
