f7e22d496751b555152a55ff44851f14ae27fc3b
nowotny <nowotnypl@gmail.com>
2011-02-17 14:00:52 +0000

    Added polish translation language file

---
+++ i18n/pl-message.php
@@ -0,0 +1,53 @@
+<?php
+
+    /**
+     * Polish language file for plugin tinymce
+     *
+     * @package Plugins
+     * @subpackage tinymce
+     *
+     * @author Piotr Łukaszewski <nowotnypl@gmail.com>
+     * @version Wolf 0.7.3
+     * @version tinymce 3.5.0     
+     */
+
+    return array(
+	'Absolute path to images on disk.<br/>For example: <code>/home/user/www/public/images</code>' => 'Ścieżka bezwzględna do zdjęć na dysku<br />Na przykład: <code>/home/user/www/public/images</code>',
+	'Always true.' => 'Zawsze włączone.',
+	'Codeprotect and Pagebreak' => '',
+	'Documentation' => 'Dokumentacja',
+	'Images directory:' => 'Katalog zdjęć:',
+	'Images dropdown function' => 'Menu listowania zdjęć',
+	'Images listing example' => '',
+	'In the popup that appears: ' => '',
+	'Internal pages dropdown function' => 'Menu listowania stron',
+	'Internal pages listing' => '',
+	'List <strong>hidden</strong> pages:' => 'Pokaż <strong>ukryte</strong> strony:',
+	'List <strong>published</strong> pages:' => 'Pokaż <strong>opublikowane</strong> strony:',
+	'Note' => '',
+	'Only lists <strong>hidden</strong> pages that are <strong>UN</strong>protected.' => 'Pokazuje tylko takie <strong>ukryte</strong> strony, które <strong>nie</strong> są chronione.',
+	'Optional' => '',
+	'Plugin' => 'Wtyczka',
+	'Preview function' => 'Plik CSS dla funkcji podglądu',
+	'Preview function example' => '',
+	'Relative URI to a CSS file.<br/>For example: <code>/public/layouts/mylayout/mystylesheet.css</code>' => 'Ścieżka względna do pliku CSS.<br />Na przykład: <code>/public/layouts/mylayout/mystylesheet.css</code>',
+	'Relative URI to images depending on where Wolf CMS was installed.<br/>For example: <code>/public/images</code>' => 'Ścieżka względna do katalogu zdjęć relatywnie do miejsca gdzie został zainstalowany WolfCMS.<br />Na przykład: <code>/public/images</code>',
+	'Save' => 'Zapisz',
+	'Settings' => 'Ustawienia',
+	'TinyMCE - plugin settings copied from old installation.' => 'TinyMCE - ustawienia skopiowane z poprzedniej instalacji.',
+	'TinyMCE - plugin settings initialized.' => 'TinyMCE - ustawienia zainicjowane.',
+	'TinyMCE - plugin settings updated.' => 'TinyMCE - ustawienia uaktualnione.',
+	'TinyMCE - unable to store plugin settings!' => 'TinyMCE - nie udało się zapisać ustawień!',
+	'TinyMCE - upgrade need detected earlier, but unable to retrieve table information!' => 'TinyMCE - wykryto wcześniejszą próbę uaktualnienia lecz nie udało się uzyskać informacji o tabeli!',
+	'TinyMCE - upgrade needed, but invalid upgrade scenario detected!' => 'TinyMCE - potrzebne uaktualnienie lecz wykryto nieprawidłowy schemat uaktualnienia!',
+	'TinyMCE - upgrade needed, but unable to retrieve old settings!' => 'TinyMCE - potrzebne uaktualnienie lecz nie udało się odczytać poprzednich ustawień!',
+	'TinyMCE Editor plugin' => 'TinyMCE Editor',
+	'TinyMCE settings' => 'TinyMCE - Konfiguracja',
+	'URI to CSS file:' => 'Ścieżka do pliku CSS:',
+	'URI to images:' => 'Ścieżka do katalogu zdjęć:',
+	'form was not posted.' => 'formularz nie został wysłany',
+	'one of the fields was empty, please try again!' => 'jedno z pól formularza jest puste, spróbuj ponownie!',
+	'plugin settings not saved!' => 'ustawienia nie zostały zapisane!',
+	'plugin settings saved.' => 'ustawienia zostały zapisane',
+	'unable to retrieve plugin settings.' => 'nie udało się odczytać ustawień.',
+    );
