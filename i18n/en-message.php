f76cef9a3bd7b251a32f63d1890fcf37db66c912
Martijn <martijn.niji@gmail.com>
2013-10-22 11:46:17 +0200

    Add missing en-message.php file

---
+++ i18n/en-message.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Wolf CMS archive plugin language file
+ *
+ * @package Translations
+ */
+
+return array(
+    'TinyMCE - upgrade needed, but invalid upgrade scenario detected!' => 'TinyMCE - upgrade needed, but invalid upgrade scenario detected!',
+    'TinyMCE - upgrade need detected earlier, but unable to retrieve table information!' => 'TinyMCE - upgrade need detected earlier, but unable to retrieve table information!',
+    'TinyMCE - upgrade needed, but unable to retrieve old settings!' => 'TinyMCE - upgrade needed, but unable to retrieve old settings!',
+    'TinyMCE - plugin settings copied from old installation.' => 'TinyMCE - plugin settings copied from old installation.',
+    'TinyMCE - plugin settings initialized.' => 'TinyMCE - plugin settings initialized.',
+    'TinyMCE - unable to store plugin settings!' => 'TinyMCE - unable to store plugin settings!',
+    'TinyMCE - plugin settings updated.' => 'TinyMCE - plugin settings updated.',
+    'TinyMCE - unable to store plugin settings!' => 'TinyMCE - unable to store plugin settings!',
+    'unable to retrieve plugin settings.' => 'unable to retrieve plugin settings.',
+    'form was not posted.' => 'form was not posted.',
+    'one of the fields was empty, please try again!' => 'one of the fields was empty, please try again!',
+    'plugin settings saved.' => 'plugin settings saved.',
+    'plugin settings not saved!' => 'plugin settings not saved!',
+    'Documentation' => 'Documentation',
+    'Codeprotect and Pagebreak' => 'Codeprotect and Pagebreak',
+    'Internal pages listing' => 'Internal pages listing',
+    'In the popup that appears: ' => 'In the popup that appears: ',
+    'Images listing example' => 'Images listing example',
+    'Note' => 'Note',
+    'Preview function example' => 'Preview function example',
+    'Optional' => 'Optional',
+    'Documentation' => 'Documentation',
+    'Settings' => 'Settings',
+    'TinyMCE Editor plugin' => 'TinyMCE Editor plugin',
+    'Plugin' => 'Plugin',
+    'TinyMCE settings' => 'TinyMCE settings',
+    'Internal pages dropdown function' => 'Internal pages dropdown function',
+    'List <strong>published</strong> pages:' => 'List <strong>published</strong> pages:',
+    'Always true.' => 'Always true.',
+    'List <strong>hidden</strong> pages:' => 'List <strong>hidden</strong> pages:',
+    'Only lists <strong>hidden</strong> pages that are <strong>UN</strong>protected.' => 'Only lists <strong>hidden</strong> pages that are <strong>UN</strong>protected.',
+    'Images dropdown function' => 'Images dropdown function',
+    'Images directory:' => 'Images directory:',
+    'Absolute path to images on disk.<br/>For example: <code>/home/user/www/public/images</code>' => 'Absolute path to images on disk.<br/>For example: <code>/home/user/www/public/images</code>',
+    'URI to images:' => 'URI to images:',
+    'Relative URI to images depending on where Wolf CMS was installed.<br/>For example: <code>/public/images</code>' => 'Relative URI to images depending on where Wolf CMS was installed.<br/>For example: <code>/public/images</code>',
+    'Preview function' => 'Preview function',
+    'URI to CSS file:' => 'URI to CSS file:',
+    'Relative URI to a CSS file.<br/>For example: <code>/public/layouts/mylayout/mystylesheet.css</code>' => 'Relative URI to a CSS file.<br/>For example: <code>/public/layouts/mylayout/mystylesheet.css</code>',
+    'Save' => 'Save',
+);
